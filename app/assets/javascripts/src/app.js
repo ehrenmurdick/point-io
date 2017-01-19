d3132584e25424e70ac76617444a0afc65c5d899
Peter Alfvin <palfvin@gmail.com>
2017-01-19 09:16:14 -0800

    WIP - cycle app now making http requests

--- app/assets/javascripts/src/app.js
+++ app/assets/javascripts/src/app.js
@@ -1,12 +1,60 @@
-import {div} from '@cycle/dom'
+import {div, button} from '@cycle/dom'
 import Rx from 'rxjs'
 
+function model(intent, responses) {
+  return {
+    buttonDisabled$: intent.buttonClick$.map(() => {
+      return true
+    }).merge(responses.partyResponse$.map(() => { return false })),
+    createParty$: intent.buttonClick$,
+    showPartyNumber$: responses.partyResponse$.map((response) => {
+      console.log(response)
+      return response.body.id})
+  }
+}
+
+function responses(sources) {
+  return {
+    partyResponse$: sources.HTTP.select('party').flatMap((e) => {return e})
+  }
+}
+
+function intent(sources) {
+  return {
+    buttonClick$: sources.DOM.select('button').events('click')
+  }
+}
+
+function view(model) {
+  return Rx.Observable.combineLatest(model.buttonDisabled$.startWith(false), model.showPartyNumber$, (disabled, partyNumber) => {
+    console.log(partyNumber)
+    return div('.stuff', {}, [
+      div('.party', {}, partyNumber),
+      button('.party', {attrs: {disabled}}, 'Create party')
+    ])
+  }).startWith(button('.party', {}, 'Create party'))
+}
+
+function httpRequests(model) {
+  return model.createParty$.map(() => { return {
+    url: 'http://localhost:3000/parties.json', // GET method by default
+    method: 'POST',
+    category: 'party',
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    }
+  } })
+}
+
 export function App (sources) {
-  const vtree$ = Rx.Observable.of(
-    div('My Awesome Cycle.js app')
-  )
+  const i = intent(sources)
+  const r = responses(sources)
+  const m = model(i, r)
+  const vtree$ = view(m)
+  const requests = httpRequests(m)
   const sinks = {
-    DOM: vtree$
+    DOM: vtree$,
+    HTTP: requests
   }
   return sinks
 }
--- app/assets/javascripts/src/index.js
+++ app/assets/javascripts/src/index.js
@@ -1,11 +1,13 @@
 import {run} from '@cycle/rxjs-run'
 import {makeDOMDriver} from '@cycle/dom'
+import {makeHTTPDriver} from '@cycle/http'
 import {App} from './app'
 
 const main = App
 
 const drivers = {
-  DOM: makeDOMDriver('#app')
+  DOM: makeDOMDriver('#app'),
+  HTTP: makeHTTPDriver()
 }
 
 run(main, drivers)
